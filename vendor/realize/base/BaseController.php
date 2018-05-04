fb9e5d359aa8d64f21c33bdebaecbdadaa339a72
IdZimin <i.d.zimin.bit@gmail.com>
2018-05-04 14:26:15 +0300

    Реализован метод формирующий полное имя файла (\/)(0~0)(\/)

--- vendor/realize/base/BaseController.php
+++ vendor/realize/base/BaseController.php
@@ -13,4 +13,27 @@ use realize\abstracts\AbstractController;
 
 class BaseController extends AbstractController
 {
+    protected function findViewFile(string $view, string $callerClass): string
+    {
+        // TODO: Implement findViewFile() method.
+        $rep = $_SERVER['DOCUMENT_ROOT'];
+        $callerClass = substr_replace($callerClass, "", -10, 10 );
+        $callerClass = lcfirst($callerClass);
+        $way = $rep . "/views/" . $callerClass . "/" . $view . ".php";
+
+
+        try {
+            if (!(file_exists($way))) {
+                throw new \Exception("Файл не найден");
+            }
+
+
+            var_dump($way);
+            return $way;
+
+        } catch (\Exception $e) {
+            echo "Ошибка: " . $e->getMessage();
+        }
+
+    }
 }
