0c878af764b15acf27475bb9ab8d8d1c4e182f55
IdZimin <i.d.zimin.bit@gmail.com>
2018-05-07 12:05:48 +0300

    реализация метдов renderCss и renderJs подключающие css и js файлы

--- vendor/realize/base/BaseAssetManager.php
+++ vendor/realize/base/BaseAssetManager.php
@@ -14,4 +14,37 @@ use realize\abstracts\AbstractAssetsManager;
 class BaseAssetManager extends AbstractAssetsManager
 {
 
+    /**
+     * @return string
+     */
+    public function renderCss(): string
+    {
+        // TODO: Implement renderCss() method.
+        $str = '';
+        $count = count($this->css);
+
+        for($i = 0; $i < $count; $i++) {
+            $elem = $this->css[$i];
+            $str = $str . '<link href=' . $elem . ' type="text/css" rel="stylesheet">';
+        }
+
+        return $str;
+
+    }
+
+    public function renderJs(): string
+    {
+        // TODO: Implement renderJs() method.
+        $str = '';
+
+        foreach ($this->depends as $elem) {
+            $str = $str . '<script type="text/javascript" src="' . $elem . '"></script>';
+        }
+
+        foreach ($this->js as $elem) {
+            $str = $str . '<script type="text/javascript" src="' . $elem . '"></script>';
+        }
+
+        return $str;
+    }
 }
