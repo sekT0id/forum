af18f0fe5392a8d253dd17dc770193befd67ec2e
IdZimin <i.d.zimin.bit@gmail.com>
2018-05-07 16:51:26 +0300

    Вёстка формы. Связь модели и элементов формы.

--- vendor/realize/base/BaseModel.php
+++ vendor/realize/base/BaseModel.php
@@ -12,4 +12,39 @@ use realize\abstracts\AbstractModel;
 
 class BaseModel extends AbstractModel
 {
+
+    public $e_mail;
+    public $password;
+    public $confirm_password;
+    public $user_name;
+    public $sex;
+    public $knownJs;
+    public $knownCpp;
+    public $knownPhp;
+    public $about;
+
+    public function load(array $data) : bool
+    {
+        // TODO: Implement load() method.
+
+        var_dump($data);
+        $i = 0;
+
+        foreach ($data as $key => $value) {
+
+            if (property_exists(self::class, $key)) {
+                $this->$key = $value;
+                $i++;
+            }
+        }
+
+
+        var_dump($this->known_languages);
+
+        if ($i > 0) {
+            return true;
+        }
+
+        return false;
+    }
 }
--- views/site/index.php
+++ views/site/index.php
@@ -2,7 +2,9 @@
 /**
  * @var $model
  */
+namespace realize\base;
 ?>
+
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
     php
@@ -10,52 +12,52 @@
 
 <main role="main" class="container">
 
-    <div class="starter-template">
-        <h1>php</h1>
-    </div>
-
-    <div class="card text-white bg-success mb-8">
-        <div class="card-header"><h4>Задание 1</h4></div>
-        <div class="card-body">
-            <h5 class="card-title">Реализовать метод load()</h5>
-
-            <p class="card-text">В своей базовой моделе необходимо реализовать метод load</p>
-            <p class="card-text">Описание находится в абстрактном классе.</p>
+    <form method="POST" action="index.php">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" name="e_mail" aria-describedby="emailHelp" placeholder="Почта" value="<?php echo $model->e_mail ?>">
         </div>
-    </div>
-
-    <br>
-
-    <div class="card text-white bg-secondary mb-8">
-        <div class="card-header"><h4>Задание 2</h4></div>
-        <div class="card-body">
-            <h5 class="card-title">
-                Сверстать форму регистрации пользователя <br>
-                twitter bootstrap 3 или twitter bootstrap 4
-            </h5>
-
-            <p class="card-text"><b>Поля:</b></p>
-            <p class="card-text">e-mail: почта пользователя</p>
-            <p class="card-text">password: пароль</p>
-            <p class="card-text">confirm password: подтверждение пароля</p>
-            <p class="card-text">user name: имя пользователя</p>
-            <p class="card-text">sex: пол</p>
-            <p class="card-text">known languages: знакомство с языками программирования</p>
-            <p class="card-text">about: немного текста о себе</p>
+        <div class="form-group">
+            <label for="exampleInputPassword">Пароль</label>
+            <input type="password" class="form-control" id="exampleInputPassword" name="password" placeholder="Пароль" value="<?php echo $model->password ?>">
         </div>
-    </div>
-
-    <br>
-
-    <div class="card text-white bg-secondary mb-8">
-        <div class="card-header"><h4>Задание 3</h4></div>
-        <div class="card-body">
-            <h5 class="card-title">Заполнить форму результатами обработки метода load()</h5>
-
-            <p class="card-text">В siteController происходит проброс формы в этот view.</p>
-            <p class="card-text">Привязать поля формы к полям модели.</p>
-            <p class="card-text">Подсказка $model->attribute</p>
+        <div class="form-group">
+            <label for="exampleInputPassword">Подтвердите пароль</label>
+            <input type="password" class="form-control" id="exampleInputPassword" name="confirm_password" placeholder="Пароль" value="<?php echo $model->confirm_password ?>">
         </div>
-    </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput">Логин</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" name="user_name" placeholder="Введите логин" value="<?php echo $model->user_name ?>">
+        </div>
+        <label for="formGroupExampleInput">Ваш пол</label>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="sex" id="inlineRadio1" value="sex_m" <?php echo $model->sex == 'sex_m' ? 'checked' : '' ?>>
+            <label class="form-check-label" for="inlineRadio1">М</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="sex" id="inlineRadio2" value="sex_w" <?php echo $model->sex == 'sex_w' ? 'checked' : '' ?>>
+            <label class="form-check-label" for="inlineRadio2">Ж</label>
+        </div>
+        <div class="form-check">
+            <label class="form-check-label" for="exampleCheck1">Cзнакомство с языками программирования</label>
+        </div>
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="customCheck1" name="knownJs" <?php echo $model->knownJs == 'on' ? 'checked' : '' ?>>
+            <label class="custom-control-label" for="customCheck1">PHP</label>
+        </div>
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="customCheck2" name="knownPhp" <?php echo $model->knownPhp == 'on' ? 'checked' : '' ?>>
+            <label class="custom-control-label" for="customCheck2">JS</label>
+        </div>
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="customCheck3" name="knownCpp" <?php echo $model->knownCpp == 'on' ? 'checked' : '' ?>>
+            <label class="custom-control-label" for="customCheck3">C++</label>
+        </div>
+        <div class="form-group">
+            <label for="comment">О себе:</label>
+            <textarea class="form-control" rows="5" id="comment" name="about"><?php echo $model->about ?></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 
 </main>
