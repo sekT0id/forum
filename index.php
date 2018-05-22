497d66d419c49d846fdd5de30f99245a8344b0bf
aleksandr.prusov <aleksandr.prusov@sokolov.ru>
2018-05-22 11:19:42 +0300

    Задание 4.

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
+require 'vendor/realize/interfaces/Db.php';
+
 require 'vendor/realize/abstracts/AbstractController.php';
 require 'vendor/realize/abstracts/AbstractView.php';
 require 'vendor/realize/abstracts/AbstractAssetsManager.php';
 require 'vendor/realize/abstracts/AbstractModel.php';
+require 'vendor/realize/abstracts/AbstractDb.php';
 
 require 'vendor/realize/base/BaseController.php';
 require 'vendor/realize/base/BaseView.php';
 require 'vendor/realize/base/BaseAssetManager.php';
 require 'vendor/realize/base/BaseModel.php';
+require 'vendor/realize/base/Mysql.php';
+
+require 'models/Users.php';
 
 require 'assets/AppAsset.php';
 
--- views/site/index.php
+++ views/site/index.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * @var $user
  * @var $model
  */
 ?>
@@ -12,48 +13,8 @@
         <h1>php</h1>
     </div>
 
-    <div class="card text-white bg-success mb-8">
-        <div class="card-header"><h4>Задание 1</h4></div>
-        <div class="card-body">
-            <h5 class="card-title">Реализовать метод load()</h5>
-
-            <p class="card-text">В своей базовой моделе необходимо реализовать метод load</p>
-            <p class="card-text">Описание находится в абстрактном классе.</p>
-        </div>
-    </div>
-
-    <br>
-
     <div class="card text-white bg-secondary mb-8">
-        <div class="card-header"><h4>Задание 2</h4></div>
-        <div class="card-body">
-            <h5 class="card-title">
-                Сверстать форму регистрации пользователя <br>
-                twitter bootstrap 3 или twitter bootstrap 4
-            </h5>
-
-            <p class="card-text"><b>Поля:</b></p>
-            <p class="card-text">e-mail: почта пользователя</p>
-            <p class="card-text">password: пароль</p>
-            <p class="card-text">confirm password: подтверждение пароля</p>
-            <p class="card-text">user name: имя пользователя</p>
-            <p class="card-text">sex: пол</p>
-            <p class="card-text">known languages: знакомство с языками программирования</p>
-            <p class="card-text">about: немного текста о себе</p>
-        </div>
-    </div>
-
-    <br>
-
-    <div class="card text-white bg-secondary mb-8">
-        <div class="card-header"><h4>Задание 3</h4></div>
-        <div class="card-body">
-            <h5 class="card-title">Заполнить форму результатами обработки метода load()</h5>
-
-            <p class="card-text">В siteController происходит проброс формы в этот view.</p>
-            <p class="card-text">Привязать поля формы к полям модели.</p>
-            <p class="card-text">Подсказка $model->attribute</p>
-        </div>
+        <?php var_dump($user);?>
     </div>
 
 </main>
